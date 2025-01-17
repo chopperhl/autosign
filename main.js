81dd007895b450082f7ab31d685b75b607b57497
chopperhl <chopperhl@qq.com>
2025-01-18 05:35:31 +0800

    init

--- main.js
+++ main.js
@@ -25,25 +25,21 @@ const checkin= async (url,cookie) => {
 const main = async () => {
   const cookie1 = process.env.COOKIE_1
   const url1 = process.env.URL_1
-  await checkin(url1,cookie1)
+  console.log(await checkin(url1,cookie1))
 
-  console.log("COMPLETE_1")
   const cookie2 = process.env.COOKIE_2
   const url2 = process.env.URL_2
-  await checkin(url2,cookie2)
+  console.log(await checkin(url2,cookie2))
 
-  console.log("COMPLETE_2")
 
   const cookie3 = process.env.COOKIE_3
   const url3 = process.env.URL_3
-  await checkin(url3,cookie3)
+  console.log(await checkin(url3,cookie3))
 
-  console.log("COMPLETE_3")
 
   const cookie4 = process.env.COOKIE_4
   const url4 = process.env.URL_4
-  await checkin(url4,cookie4)
-  console.log("COMPLETE_4")
+  console.log(await checkin(url4,cookie4))
 }
 
 main()
